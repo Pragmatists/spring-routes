fbb67117917a1663103b2c5207820d88611f1f34
Lukasz Strzelecki <lu.strzelecki@gmail.com>
2014-11-27 12:12:20 +0100

    First Rest Test with SpringBoot

---
+++ examples/src/main/java/org/springroutes/BasicApplication.java
@@ -0,0 +1,13 @@
+package org.springroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+//@ComponentScan
+public class BasicApplication {
+
+}
